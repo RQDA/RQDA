4c2df6db8be97bde34a6fb78704175064ac167ba
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2020-05-23 19:21:10 +0200

    create what is necessary and avoid the bin file
    ToDo: export settings to database

---
+++ R/rqda-env.R
@@ -0,0 +1,14 @@
+
+button <- new.env(parent = emptyenv())
+.rqda <- new.env(parent = emptyenv())
+
+.rqda$back.col <- "gold"
+.rqda$BOM <- FALSE
+.rqda$TOR <- "unconditional"
+.rqda$codeMark.col <- "green"
+.rqda$codingTable <- "coding"
+.rqda$isLaunched <- FALSE
+.rqda$owner <- "default"
+.rqda$encoding <- "unknown"
+.rqda$back.col <- "gold"
+.rqda$fore.col <- "blue"
--- R/zzz.R
+++ R/zzz.R
@@ -1,28 +1,34 @@
 ## part of RQDA project
 ## by ronggui HUANG
 
+
 .onAttach <- function(...) {
   ## use .onLoad/.onAttach rather than .First.lib when there is namespace
   ## Refer R news 2003-1 for details about name space
+
   optOld <- options()
+
   ## if (is.null(getOption("widgetCoordinate")))
   ##  options(widgetCoordinate=c(400,2))
   ## cordinate of ViewFunWidget
 
   #if (is.null(getOption("widgetSize")))
-        options(widgetSize = c(gdkScreenWidth()*.9,
-                               gdkScreenHeight()*.9))
+  options(widgetSize = c(gdkScreenWidth()*.9,
+                         gdkScreenHeight()*.9))
 
   options(andMethod=c("overlap","exact","inclusion"))
   assign("optOld",optOld,envir=.rqda)
   if (interactive()) {
-     packageStartupMessage(
-       "\nUse 'RQDA()' to start the programme.\n", domain = "R-RQDA")
-     RQDA()
-   }
+    packageStartupMessage(
+      "\nUse 'RQDA()' to start the programme.\n", domain = "R-RQDA")
+    RQDA()
+  }
 }
 
 .onUnload <- function(...){
   cat("Bye, RQDA is unloaded.\n")
   options(.rqda$optOld)
+
+  rm(button)
+  rm(.rqda)
 }
