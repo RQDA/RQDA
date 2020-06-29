829261cb17551aa93590635c199d50fac6c1eb83
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2020-06-29 19:46:01 +0200

    cleanup and avoid error if no file is selected

--- R/saveAs.R
+++ R/saveAs.R
@@ -1,34 +1,58 @@
-saveAsButt <- function(label=gettext("Save Project As ...", domain = "R-RQDA"), container){
-    saveAsB <- gbutton(text=label, container=container, handler=function(h, ...){
-        saveAs()
-    }
-            )
-   enabled(saveAsB) <- FALSE
-   assign("saveAsB",saveAsB, envir=button)
+saveAsButt <- function(label=rqda_txt("Save Project As ..."),
+                       container) {
+
+    saveAsB <- gbutton(text=label, container=container,
+                       handler=function(h, ...){
+                           saveAs()
+                       })
+
+    enabled(saveAsB) <- FALSE
+    assign("saveAsB", saveAsB, envir=button)
 }
 
 saveAs <- function(newpath=NULL) {
     oldpath <- dbGetInfo(.rqda$qdacon)$dbname
-    if (Encoding(oldpath)=="unknown") Encoding(oldpath) <- "UTF-8"
+
+    if (Encoding(oldpath)=="unknown")
+        Encoding(oldpath) <- "UTF-8"
+
     if (is.null(newpath)) {
-        newpath <- gfile(type="save",text = gettext("Type a new file name and click OK.", domain = "R-RQDA"),filter = list("RQDA" = list(patterns = c("*.rqda$"))))
-        if (Encoding(newpath) != "UTF-8") Encoding(newpath) <- "UTF-8"
+        newpath <- gfile(type="save",
+                         text = rqda_txt("Type a new file name and click OK."),
+                         filter = list("RQDA" = list(patterns = c("*.rqda$"))))
+
+        # if no file selected, return nothing
+        if (identical(newpath, character(0)))
+            return(0)
+
+        if (Encoding(newpath) != "UTF-8")
+            Encoding(newpath) <- "UTF-8"
+
         newpath <- sprintf("%s.rqda", newpath)
     }
+
     override <- TRUE
     if (fexist <- file.exists(newpath)) {
-        override <- gconfirm(gettext("Overwrite existing project?", domain = "R-RQDA"),icon="warning")
+        override <- gconfirm(rqda_txt("Overwrite existing project?"),
+                             icon="warning")
         if (file.access(newpath, 2) != 0 && override) {
             override <- FALSE
-            gmessage(gettext("You have no write permission to overwrite it.", domain = "R-RQDA"),container=TRUE,icon="error")
+            gmessage(rqda_txt("You have no write permission to overwrite it."),
+                     container=TRUE, icon="error")
         }
     }
+
     if (!fexist | override ){
         succeeded <- file.copy(from=oldpath, to=newpath,overwrite=override)
     }
-    if (!succeeded) gmessage(gettext("Failed to save the project to the new location.", domain = "R-RQDA"),container=TRUE,icon="error")
+
+    if (!succeeded)
+        gmessage(rqda_txt("Failed to save the project to the new location."),
+                 container=TRUE, icon="error")
+
     closeProjBF()
-    ## this must be placed before closeProject() because .fnames_rqda[] <- NULL will triger Clicked handler
+    ## this must be placed before closeProject() because .fnames_rqda[] <- NULL
+    ## will triger Clicked handler
     closeProject()
     enabled(button$cloprob) <- FALSE
     openProject(path=newpath,updateGUI=TRUE)
